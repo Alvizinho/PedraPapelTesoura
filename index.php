8a42bee01d28891cd1e985ab601eb8ff13d16684
credendim <130668967+credendim@users.noreply.github.com>
2023-07-28 16:15:23 -0300

    commit

--- index.php
+++ index.php
@@ -5,14 +5,15 @@
     <meta name="viewport" content="width=a, initial-scale=1.0">
     <title>Jokenpo</title>
 </head>
-<body>
+<body align="center">
+    <h1>Escolha!</h1>
     <form action="resultado.php" method="get">
         <input type="radio" name="valor" id="pedra" value="0">
         <label for="pedra">Pedra</label>
         <input type="radio" name="valor" id="papel" value="1">
         <label for="papel">Papel</label>
         <input type="radio" name="valor" id="tesoura" value="2">
-        <label for="tesoura">Tesoura</label></br>
+        <label for="tesoura">Tesoura</label></br><br>
         <input type="submit">
     </form>
     
--- resultado.php
+++ resultado.php
@@ -3,29 +3,56 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
     <title>Resultado</title>
 </head>
-<body>
+<body align="center">
+    <h1>Resultado!</h1>
+
     <?php
-        $valor = ($_GET["valor"]);
-        $bot = rand(0,2);
+            $valor = $_GET["valor"];
+            $bot = rand(0,2);
+    
+            //Escrever na pagina o que o jogador selecionou
+            if ($valor == 0){
+                echo "Pedra";
+            }elseif ($valor == 1){
+                echo "Papel";
+            }else {
+                echo "Tesoura";
+            }
 
-        if ($valor == 1 and $bot == 0){
-            echo "Ganhou";
-            echo "$bot";
-        }if ($valor == 2 and $bot == 1){
-            echo "Ganhou";
-            echo "$bot";
-        }if ($valor == 0 and $bot == 2){
-            echo "Ganhou";
-            echo "$bot";
-        }if ($valor == $bot){
-            echo "empate";
-        }else {
-            echo "perdeu";
-            echo "$bot";
-        }
+            //Escrever na pagina o que o bot selecionou
+            if ($bot == 0){
+                echo "Pedra";
+            }elseif ($bot == 1){
+                echo "Papel";
+            }else {
+                echo "Tesoura";
+            }
 
+            //Laço de repetição para verfificar o resultado do jogo
+            //Papel contra pedra
+            if ($valor == 1 and $bot == 0){
+                echo "Ganhou!";
+            }
+            //Tesoura contra papel
+            elseif ($valor == 2 and $bot == 1){
+                echo "Ganhou!";
+            }
+            //Pedra contra tesoura
+            elseif ($valor == 0 and $bot == 2){
+                echo "Ganhou!";
+            }
+            //Empate
+            elseif ($valor == $bot){
+                echo "Empate!";
+            }
+            //Derrota
+            else {
+                echo "Perdeu!";
+            }
     ?>
+    <a href="index.php" id="voltar"><button type="" <center> Voltar </center></a>
 </body>
 </html>
